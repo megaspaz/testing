413286e5ed206d8179714dc12421f0eae053b42e
megaspaz <megaspaz2k7@gmail.com>
2018-07-16 21:41:30 -0700

    initial skeleton of api impl. Restructured layout because of it.

--- support/env.rb
+++ support/env.rb
@@ -3,6 +3,7 @@ require 'rspec/matchers'
 require 'colored'
 require 'rbconfig'
 require 'require_all'
+require 'rest-client'
 require 'selenium-webdriver'
 require 'yaml'
 require_rel '../lib/*.rb'
@@ -12,9 +13,12 @@ ENV['SELENIUM_BROWSER'] ||= 'firefox'
 ENV['DEBUG_MODE'] ||= 'false'
 ENV['VIEW_IMPL'] ||= 'desktop_web'
 
-if ENV['VIEW_IMPL'] =~ /^(desktop|mobile|tablet)_web$/
-  SeleniumDriver.get_driver
-end
+puts Colored.colorize(
+  "OS: #{ENV['OS'].upcase}\nBROWSER: #{ENV['VIEW_IMPL'] == 'api' ? 'NIL' : ENV['SELENIUM_BROWSER'].upcase}\n" +
+    "VIEW: #{ENV['VIEW_IMPL'].upcase}\nDEBUG: #{ENV['DEBUG_MODE'].upcase}").bold.blue
+
+SeleniumDriver.get_driver if ENV['VIEW_IMPL'] =~ /^(desktop|mobile|tablet)_web$/
+$api_client ||= ApiClient.new if ENV['VIEW_IMPL'] =~ /^(desktop_web|mobile_web|tablet_web|api)$/
 
 # Take a screenshot on fail.
 After('@desktop_web or @mobile_web or @tablet_web') do |scenario|
@@ -30,6 +34,7 @@ at_exit do
   case ENV['VIEW_IMPL']
   when /^(desktop|mobile|tablet)_web$/
     if !ENV['DEBUG_MODE'].to_bool
+      $api_client = nil
       puts Colored.colorize(
         'Safari requires manually quitting the browser...').bold.yellow if ENV['SELENIUM_BROWSER'] == 'safari'
       $driver&.quit
@@ -40,5 +45,7 @@ at_exit do
           "Browser is always killed -> https://github.com/SeleniumHQ/selenium/issues/742\n" +
           "Safari does not work for some other reason...").bold.yellow
     end
+  when 'api'
+    $api_client = nil
   end
 end
