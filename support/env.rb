5618cd12c932bc2cadb55029a217865b0d8bd875
megaspaz <megaspaz2k7@gmail.com>
2018-08-03 13:29:55 -0700

    rubify

--- support/env.rb
+++ support/env.rb
@@ -24,7 +24,7 @@ puts Colored.colorize(
     "VIEW: #{ENV['VIEW_IMPL'].upcase}\nDEBUG: #{ENV['DEBUG_MODE'].upcase}").bold.blue
 
 # Create output dir if it doesn't exist.
-Dir.mkdir('results') if !Dir.exist?('results')
+Dir.mkdir('results') unless Dir.exist?('results')
 
 SeleniumDriver.get_driver if ENV['VIEW_IMPL'] =~ /^(desktop|mobile|tablet)_web$/
 AppiumDriver.get_driver if ENV['VIEW_IMPL'] =~ /^(mobile|tablet)_app_(android|ios)$/
