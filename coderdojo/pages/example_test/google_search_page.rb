1561cfd4f167f920fed191f13af2e8db92035865
megaspaz <megaspaz2k7@gmail.com>
2018-09-01 15:59:36 -0700

    update cukes. fix test due to changes.

--- coderdojo/pages/example_test/google_search_page.rb
+++ coderdojo/pages/example_test/google_search_page.rb
@@ -1,17 +1,18 @@
 class GoogleSearchPage < BasePage
 
-  SEARCH_BOX        = { css: 'div#sb_ifc0 div#gs_lc0 input#lst-ib'  }
-  SEARCH_BOX_SUBMIT = { css: 'div.jsb input[value="Google Search"]' }
+  SEARCH_BOX        = { css: 'input[name="q"]'  }
+  SEARCH_BOX_SUBMIT = { css: 'input[type="submit"]' }
 
   @page_url = 'https://www.google.com'
   def initialize(driver)
     super(driver)
-    wait_for(5) { find(SEARCH_BOX).displayed? && find(SEARCH_BOX_SUBMIT).displayed? }
+    wait_for(5) { find(SEARCH_BOX).displayed? }
   end
 
   def search_for(search_for)
     type(SEARCH_BOX, search_for)
     type_meta(:escape)
+    wait_for(5) { find(SEARCH_BOX_SUBMIT).displayed? }
     click_on(SEARCH_BOX_SUBMIT)
   end
 end
